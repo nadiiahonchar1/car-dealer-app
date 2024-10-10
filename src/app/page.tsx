8d84bfe3ee814cae59806c86838b33385f2f00cf
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-10-10 13:43:50 +0300

    added select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
-import Image from "next/image";
+// import Image from "next/image";
+"use client";
 
-export default function Home() {
+export default async function Home() {
+  let data = await fetch(
+    "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+  );
+  let posts = await data.json();
+
+  const handleChange = (e: any) => {
+    console.log(e);
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -9,7 +19,19 @@ export default function Home() {
           The application will allow users to filter vehicles by type and model
           year, and view the results on a separate page.
         </p>
-        <div className="flex gap-4 items-center flex-col sm:flex-row"></div>
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <form>
+            <label>Please, choose your model</label>
+            <select name="model" onChange={handleChange}>
+              <option value="" disabled></option>
+              {posts.Results.map((post: any) => (
+                <option key={post.MakeId} value={post.MakeName}>
+                  {post.MakeName}
+                </option>
+              ))}
+            </select>
+          </form>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p className="flex items-center gap-2 hover:underline hover:underline-offset-4">
