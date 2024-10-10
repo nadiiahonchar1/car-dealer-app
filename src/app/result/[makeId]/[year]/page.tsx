b4b5fbf2c84aaae86d0d114616437e00188b916b
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-10-10 17:29:07 +0300

    added title

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -4,18 +4,25 @@ import { useEffect, useState } from "react";
 import { getModelByParams } from "@/app/api/fetch";
 
 export default function Cars() {
+  type model = {
+    Make_ID: number;
+    Make_Name: string;
+    Model_ID: number;
+    Model_Name: string;
+  };
+
   const [models, setModels] = useState();
   const pathname = usePathname();
-  console.log("pathname", pathname);
+  //   console.log("pathname", pathname);
   const arrPath = pathname.split("/");
-  console.log(arrPath);
+  //   console.log(arrPath);
 
   useEffect(() => {
     const data = async () => {
       try {
         const fetchedModels = await getModelByParams(arrPath[2], arrPath[3]);
         setModels(fetchedModels);
-        console.log("fetchedModels", fetchedModels.Results);
+        // console.log("fetchedModels", fetchedModels.Results);
       } catch (error) {
         console.error("Error fetching products by ID:", error);
       }
@@ -28,29 +35,26 @@ export default function Cars() {
   }
 
   return (
-    <ul>
-      {render.map((model: any) => (
-        <li key={model.Make_ID}>
-          <p>
-            <span>Make_Name: </span>
-            {model.Make_Name}
-          </p>
-          <p>
-            <span>"Model_ID: </span>
-            {model.Model_ID}
-          </p>
-          <p>
-            <span>"Model_Name: </span>
-            {model.Model_Name}
-          </p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1>Cars for the {arrPath[3]} year</h1>
+      <ul>
+        {render.map((model: model) => (
+          <li key={model.Make_ID}>
+            <p>
+              <span>Make_Name: </span>
+              {model.Make_Name}
+            </p>
+            <p>
+              <span>"Model_ID: </span>
+              {model.Model_ID}
+            </p>
+            <p>
+              <span>"Model_Name: </span>
+              {model.Model_Name}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
-// {
-//     "Make_ID": 454,
-//     "Make_Name": "BUGATTI",
-//     "Model_ID": 19517,
-//     "Model_Name": "Chiron"
-// }
