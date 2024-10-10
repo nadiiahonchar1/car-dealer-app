8a790b38b3e15b97badde340097ab29f24239cf5
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-10-10 17:47:04 +0300

    add condition to button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,8 @@ export default function Home() {
   const [targetModel, setTargetModel] = useState();
   const [targetYear, setTargetYear] = useState();
 
-  // const endpoint = process.env.NEXT_PUBLIC_BASE_URL;
   const data = new Date();
   const yearArr = [];
-  let newLink = `/result/undefined/undefined`;
 
   for (let i = 2015; i <= data.getFullYear(); i++) {
     yearArr.push(i);
@@ -48,59 +46,54 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="mx-auto font-bold">The Car Dealer App</h1>
-        <p>
-          The application will allow users to filter vehicles by type and model
-          year, and view the results on a separate page.
-        </p>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {models ? (
-            <form>
-              <label>Please, choose your model</label>
-              <select
-                name="model"
-                onChange={handleChangeModel}
-                className="border-2 border-gray-950 rounded-lg block"
-              >
-                <option value="" disabled></option>
-                {models.Results.map((model: carModel) => (
-                  <option key={model.MakeId} value={model.MakeId}>
-                    {model.MakeName}
-                  </option>
-                ))}
-              </select>
-              <label>Please, choose year</label>
-              <select
-                name="year"
-                onChange={handleChangeYear}
-                className="border-2 border-gray-950 rounded-lg block"
-              >
-                <option value="" disabled></option>
-                {yearArr.map((i) => (
-                  <option key={i} value={i}>
-                    {i}
-                  </option>
-                ))}
-              </select>
-            </form>
-          ) : (
-            <p>Something went wrong</p>
-          )}
+    <div>
+      <h1 className="mx-auto font-bold mb-8">The Car Dealer App</h1>
+      <p>
+        The application will allow users to filter vehicles by type and model
+        year, and view the results on a separate page.
+      </p>
+      <div className="flex gap-4 items-center flex-col sm:flex-row">
+        {models ? (
+          <form>
+            <label>Please, choose your model</label>
+            <select
+              name="model"
+              onChange={handleChangeModel}
+              className="border-2 border-gray-950 rounded-lg block"
+            >
+              <option value="" disabled></option>
+              {models.Results.map((model: carModel) => (
+                <option key={model.MakeId} value={model.MakeId}>
+                  {model.MakeName}
+                </option>
+              ))}
+            </select>
+            <label>Please, choose year</label>
+            <select
+              name="year"
+              onChange={handleChangeYear}
+              className="border-2 border-gray-950 rounded-lg block"
+            >
+              <option value="" disabled></option>
+              {yearArr.map((i) => (
+                <option key={i} value={i}>
+                  {i}
+                </option>
+              ))}
+            </select>
+          </form>
+        ) : (
+          <p>Something went wrong</p>
+        )}
+        {targetModel && targetYear && (
           <Link
             href={`/result/${targetModel}/${targetYear}`}
             className="border-2 border-gray-950 rounded-lg block p-2"
           >
             Go to /result/{targetModel}/{targetYear}
           </Link>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p className="flex items-center gap-2 hover:underline hover:underline-offset-4">
-          This is a test
-        </p>
-      </footer>
+        )}
+      </div>
     </div>
   );
 }
--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -13,16 +13,13 @@ export default function Cars() {
 
   const [models, setModels] = useState();
   const pathname = usePathname();
-  //   console.log("pathname", pathname);
   const arrPath = pathname.split("/");
-  //   console.log(arrPath);
 
   useEffect(() => {
     const data = async () => {
       try {
         const fetchedModels = await getModelByParams(arrPath[2], arrPath[3]);
         setModels(fetchedModels);
-        // console.log("fetchedModels", fetchedModels.Results);
       } catch (error) {
         console.error("Error fetching products by ID:", error);
       }
@@ -35,26 +32,32 @@ export default function Cars() {
   }
 
   return (
-    <>
-      <h1>Cars for the {arrPath[3]} year</h1>
-      <ul>
-        {render.map((model: model) => (
-          <li key={model.Make_ID}>
-            <p>
-              <span>Make_Name: </span>
-              {model.Make_Name}
-            </p>
-            <p>
-              <span>"Model_ID: </span>
-              {model.Model_ID}
-            </p>
-            <p>
-              <span>"Model_Name: </span>
-              {model.Model_Name}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+      <h1 className="mx-auto font-bold mb-8">
+        Cars model {render[0].Make_Name} for the {arrPath[3]} year:
+      </h1>
+      {models ? (
+        <ul>
+          {render.map((model: model) => (
+            <li key={model.Make_ID} className="flex gap-8">
+              <p>
+                <span>Make_Name: </span>
+                {model.Make_Name}
+              </p>
+              <p>
+                <span>"Model_ID: </span>
+                {model.Model_ID}
+              </p>
+              <p>
+                <span>"Model_Name: </span>
+                {model.Model_Name}
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Something went wrong</p>
+      )}
+    </div>
   );
 }
